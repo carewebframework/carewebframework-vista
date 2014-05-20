9927619bef7f362fa3196436d4ddb2b5bea0958a
Douglas Martin <dkmartin@regenstrief.org>
2014-05-19 23:14:46 -0400

    Standardize date formats.

--- org.carewebframework.vista.ui-parent/org.carewebframework.vista.ui.vitals/src/main/java/org/carewebframework/vista/ui/vitals/Display.java
+++ org.carewebframework.vista.ui-parent/org.carewebframework.vista.ui.vitals/src/main/java/org/carewebframework/vista/ui/vitals/Display.java
@@ -1,8 +1,8 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
  * http://mozilla.org/MPL/2.0/.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related Additional
  * Disclaimer of Warranty and Limitation of Liability available at
  * http://www.carewebframework.org/licensing/disclaimer.
@@ -29,6 +29,7 @@ import org.carewebframework.shell.plugins.PluginContainer;
 import org.carewebframework.ui.highcharts.Chart;
 import org.carewebframework.ui.highcharts.DashStyle;
 import org.carewebframework.ui.highcharts.DataPoint;
+import org.carewebframework.ui.highcharts.DateTimeFormatOptions;
 import org.carewebframework.ui.highcharts.Series;
 import org.carewebframework.ui.highcharts.ZoomType;
 import org.carewebframework.ui.zk.DateRangePicker;
@@ -56,59 +57,63 @@ import org.zkoss.zul.Toolbar;
  * Controller for vital measurement display.
  */
 public class Display extends Div implements PatientContext.IPatientContextEvent, IPluginEvent {
-    
+
     private static final Log log = LogFactory.getLog(Display.class);
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     private static final String[] rangeSeries = new String[] { "low", "high" };
+
+    private static final String DATE_FORMAT = "%d-%b-%y";
     
+    private static final String TIME_FORMAT = DATE_FORMAT + " %H:%M";
+
     private Chart chart;
-    
+
     private Listbox lstVitals;
-    
+
     private Listhead hdrVitals;
-    
+
     private Paging pgVitals;
-    
+
     private Slider sldVitals;
-    
+
     private Checkbox chkGrid;
-    
+
     private Checkbox chkAge;
-    
+
     private Checkbox chkPercentiles;
-    
+
     private Checkbox chkZoom;
-    
+
     private Combobox cboUnits;
-    
+
     private Button btnEnterVitals;
-    
+
     private DateRangePicker datRange;
-    
+
     private Toolbar tbarPaging;
-    
+
     private Patient patient;
-    
+
     private int selectedRow = -1;
-    
+
     private String selectedItem;
-    
+
     private final Date today = new Date();
-    
+
     private final Map<String, String> percentiles = new HashMap<String, String>();
-    
+
     private List<String> tests;
-    
+
     private BrokerSession broker;
-    
+
     private String gridRPC;
-    
+
     private String detailRPC;
-    
+
     private final int maxCols = 5;
-    
+
     public void selectData(String test, String gridRPC, String detailRPC, List<String> tests) {
         this.gridRPC = gridRPC;
         this.detailRPC = detailRPC;
@@ -116,7 +121,7 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
         committed();
         setSelectedRow(test);
     }
-    
+
     public void onCreate() {
         ZKUtil.wireController(this);
         FrameworkUtil.getAppFramework().registerObject(this);
@@ -126,14 +131,17 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
         chart.getXAxis().gridLineWidth = 0;
         chart.getYAxis().gridLineWidth = 0;
         chart.options.exporting.buttons_printButton.onclick = "cwf.print(this.container);";
-        chart.options.plotOptions.tooltip.xDateFormat = "dd-MMM-yyyy HH:mm";
+        chart.options.plotOptions.tooltip.xDateFormat = "%d-%m-%Y";
+        DateTimeFormatOptions dtlf = chart.getXAxis().dateTimeLabelFormats;
+        dtlf.setDateFormats(DATE_FORMAT);
+        dtlf.setTimeFormats(TIME_FORMAT);
         broker = VistAUtil.getBrokerSession();
         cboUnits.setSelectedIndex(0);
         datRange.setSelectedIndex(0);
         btnEnterVitals.setVisible(Entry.isEnabled());
         selectData("", "RGCWVM GRID", "RGCWVM DETAIL", null);
     }
-    
+
     private Date ageToDate(double age) {
         if (chkAge.isVisible() && chkAge.isChecked()) {
             return DateUtil.addDays(patient.getBirthDate(), (int) (age * 365.25 / 12.0), true);
@@ -141,62 +149,62 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             return new Date();
         }
     }
-    
+
     private double dateToAge(Date date) {
         double diff = date.getTime() - patient.getBirthDate().getTime();
         return diff / 2592000000.0;
     }
-    
+
     private Listcell setValue(int col, int row, String value, Object object) {
         Listcell cell = getCell(col, row);
         cell.setLabel(value);
         cell.setValue(object);
         return cell;
     }
-    
+
     private String getValue(int col, int row) {
         return getCell(col, row).getLabel();
     }
-    
+
     private Object getObject(int col, int row) {
         return getCell(col, row).getValue();
     }
-    
+
     private Listcell getCell(int col, int row) {
         Listitem item;
-        
+
         while (row >= lstVitals.getItemCount()) {
             item = new Listitem();
             item.setVisible(false);
             item.setParent(lstVitals);
         }
-        
+
         item = lstVitals.getItemAtIndex(row);
-        
+
         while (col >= item.getChildren().size()) {
             Listcell cell = new Listcell("");
             cell.setParent(item);
         }
-        
+
         return (Listcell) item.getChildren().get(col);
     }
-    
+
     private Series findSeries(String seriesName, boolean forceCreate) {
         for (Series series : chart.options.series) {
             if (seriesName.equals(series.name)) {
                 return series;
             }
         }
-        
+
         if (!forceCreate) {
             return null;
         }
-        
+
         Series series = chart.addSeries();
         series.name = seriesName;
         return series;
     }
-    
+
     private DataPoint plotData(double xVal, String value, String seriesName, String id) {
         try {
             double yVal = Double.parseDouble(value);
@@ -208,16 +216,16 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             return null;
         }
     }
-    
+
     private void plotRange(double xLow, double xHigh, String range) {
         final String pcs[] = StrUtil.split(range, "-", 2);
-        
+
         for (int i = 0; i < 2; i++) {
             String seriesName = rangeSeries[i];
             Series series = findSeries(seriesName, true);
             DataPoint low = plotData(xLow, pcs[i], seriesName, null);
             DataPoint high = plotData(xHigh, pcs[i], seriesName, null);
-            
+
             if (low == null || high == null) {
                 chart.options.series.remove(series);
             } else {
@@ -229,11 +237,11 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             }
         }
     }
-    
+
     private void plotPercentile(double xVal, String value, String series) {
         Series pctile = findSeries(series, true);
         DataPoint dp = plotData(xVal, value, series, series);
-        
+
         if (pctile.plotOptions.dashStyle == null) {
             pctile.plotOptions.marker.enabled = false;
             pctile.plotOptions.enableMouseTracking = false;
@@ -241,7 +249,7 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             pctile.plotOptions.lineWidth = 1;
         }
     }
-    
+
     private void chartData() {
         Date dateHigh = null;
         Date dateLow = null;
@@ -249,11 +257,11 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
         boolean useAge = chkAge.isVisible() && chkAge.isChecked();
         chart.clear();
         int colcount = hdrVitals.getChildren().size() - 1;
-        
+
         if (row < 0 || row >= lstVitals.getItemCount() || colcount < 0) {
             return;
         }
-        
+
         boolean hasData = false;
         String testname = getValue(0, row);
         String testid = (String) getObject(0, row);
@@ -261,24 +269,24 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
         chart.getYAxis().title.text = (String) getObject(colcount, row);
         chart.getXAxis().title.text = useAge ? "age (months)" : null;
         chart.getXAxis().type = useAge ? "linear" : "datetime";
-        
+
         for (int col = 1; col < colcount; col++) {
             Listheader hdr = (Listheader) hdrVitals.getChildren().get(col);
             FMDate date = (FMDate) hdr.getValue();
-            
+
             if (date != null) {
                 double xVal = useAge ? dateToAge(date) : date.getTime();
                 String vals[] = StrUtil.split(getValue(col, row), ";");
                 boolean newData = false;
-                
+
                 for (String val : vals) {
                     if (isBP) {
                         String pcs[] = StrUtil.split(val, "/");
-                        
+
                         if (pcs.length > 0) {
                             newData |= plotData(xVal, pcs[0], "Systolic", testname) != null;
                         }
-                        
+
                         if (pcs.length > 2) {
                             newData |= plotData(xVal, pcs[1], "Mean", testname) != null;
                             newData |= plotData(xVal, pcs[2], "Diastolic", testname) != null;
@@ -289,7 +297,7 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
                         newData |= plotData(xVal, val, testname, testname) != null;
                     }
                 }
-                
+
                 if (newData) {
                     hasData = true;
                     dateLow = dateLow == null ? date : date.getTime() < dateLow.getTime() ? date : dateLow;
@@ -297,74 +305,74 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
                 }
             }
         }
-        
+
         if (hasData) {
             double xLow = useAge ? dateToAge(dateLow) : dateLow.getTime();
             double xHigh = useAge ? dateToAge(dateHigh) : dateHigh.getTime();
             plotRange(xLow, xHigh, StrUtil.piece(getValue(colcount, row), " "));
             String pctileRPC = percentiles.get(testid);
-            
+
             if (pctileRPC != null && chkPercentiles.isChecked()) {
                 List<String> pctiles = broker.callRPCList(pctileRPC, null, testid, patient.getDomainId(),
                     DateUtils.addDays(dateLow, -3000), DateUtils.addDays(dateHigh, 3000), getDefaultUnits());
-                
+
                 for (String pctile : pctiles) {
                     String pcs[] = StrUtil.split(pctile, StrUtil.U, 3);
                     FMDate date = new FMDate(pcs[1]);
                     plotPercentile(useAge ? dateToAge(date) : date.getTime(), pcs[2], pcs[0]);
                 }
             }
-            
+
             chart.run();
         }
     }
-    
+
     /**
      * Load the string grid with data. Calls the grid RPC to retrieve data for the grid. Data is
      * returned in the following format:
-     * 
+     *
      * <pre>
-     * 
+     *
      * counts: test count^date count^result count
      * tests: control ien^test ien^test name^test abbrv^units^low norm^hi norm^percentile RPC
      * dates: date id^FM date results: date id^row #^value^result ien
-     * 
-     * For example: 
-     * 
+     *
+     * For example:
+     *
      * 8^2^7
-     * 
+     *
      * 3^3^TEMPERATURE^TMP^F^^^ 5^5^PULSE^PU^/min^60^100^
      * 15^15^RESPIRATIONS^RS^/min^^^ 4^4^BLOOD PRESSURE^BP^mmHg^90^150^
      * 1^1^HEIGHT^HT^in^^^CIAOCVVM PCTILE 2^2^WEIGHT^WT^lb^^^CIAOCVVM PCTILE
      * 21^21^PAIN^PA^^^^ 6^6^HEAD CIRCUMFERENCE^HC^in^^^CIAOCVVM PCTILE
-     * 
-     * 2^3041018.1446 
+     *
+     * 2^3041018.1446
      * 1^3041022.1446
-     * 
+     *
      * 1^2^77^^2445227 1^4^101/65^^2445224 1^5^27^^2445222 2^5^26.5^^2445220
      * 1^6^16.5^^2445223 2^6^16^^2445218 1^8^17.5^^2445225
      * </pre>
      */
     private void loadGrid() {
         chart.clear();
-        
+
         if (patient == null) {
             showMessage("No patient selected.");
             return;
         }
-        
+
         Iterator<String> data = doRPC(gridRPC, datRange.getStartDate(), datRange.getEndDate(), tests).iterator();
         percentiles.clear();
         String[] pcs = StrUtil.split(data.next(), StrUtil.U, 3);
         int testcnt = StrUtil.toInt(pcs[0]);
         int datecnt = StrUtil.toInt(pcs[1]);
         int datacnt = StrUtil.toInt(pcs[2]);
-        
+
         if (datacnt == 0 || datecnt == 0) {
             showMessage("No data available within selected range.");
             return;
         }
-        
+
         initGrid(datecnt + 2, testcnt);
         // Populate test names and units
         for (int r = 0; r < testcnt; r++) {
@@ -373,14 +381,14 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             range = "-".equals(range) ? "" : range + " ";
             setValue(0, r, WordUtils.capitalizeFully(pcs[2]), pcs[0]).setStyle("font-weight:bold");
             setValue(datecnt + 1, r, range + pcs[4], pcs[4]).setStyle("font-style:italic");
-            
+
             if (!pcs[7].isEmpty()) {
                 percentiles.put(pcs[0], pcs[7]);
             }
         }
         // Populate date headers
         Map<String, Listheader> headers = new HashMap<String, Listheader>();
-        
+
         for (int c = 1; c <= datecnt; c++) {
             pcs = StrUtil.split(data.next(), StrUtil.U, 2);
             FMDate date = new FMDate(pcs[1]);
@@ -398,15 +406,15 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             setValue(col, row, StrUtil.strAppend(getValue(col, row), pcs[2], "; "), null);
             lstVitals.getItemAtIndex(row).setVisible(true);
         }
-        
+
         lstVitals.invalidate();
         setSelectedRow(selectedItem);
     }
-    
+
     private void initGrid(int colcount, int rowcount) {
         lstVitals.getItems().clear();
         hdrVitals.getChildren().clear();
-        
+
         if (colcount > 2 && rowcount > 0) {
             tbarPaging.setVisible(true);
             sldVitals.setMaxpos(colcount - 2);
@@ -415,7 +423,7 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             pgVitals.setTotalSize(colcount - 2);
             pgVitals.setPageSize(maxCols + 1);
             getCell(colcount - 1, rowcount - 1);
-            
+
             for (int i = 1; i <= colcount; i++) {
                 Listheader lh = new Listheader();
                 lh.setVisible(i <= maxCols || i == colcount);
@@ -427,14 +435,14 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             tbarPaging.setVisible(false);
         }
     }
-    
+
     private void showMessage(String message) {
         initGrid(0, 0);
         Listcell cell = getCell(0, 0);
         cell.setLabel(message);
         cell.getParent().setVisible(true);
     }
-    
+
     private void setSelectedRow(String test) {
         if (StringUtils.isEmpty(test)) {
             setSelectedRow(-1);
@@ -447,7 +455,7 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             }
         }
     }
-    
+
     private void setSelectedRow(int index) {
         if (index < 0) {
             selectedRow = -1;
@@ -460,19 +468,19 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
             lstVitals.setSelectedIndex(index);
             chkPercentiles.setVisible(percentiles.containsKey(getObject(0, index)));
         }
-        
+
         chartData();
     }
-    
+
     private int getDefaultUnits() {
         return cboUnits.getSelectedIndex() - 1;
     }
-    
+
     private List<String> doRPC(String rpcName, Date date1, Date date2, List<String> tests) {
         // TODO: need to use encounter location
         return broker.callRPCList(rpcName, null, patient.getDomainId(), date1, date2, 0, tests, 0, getDefaultUnits());
     }
-    
+
     private void updatePaging() {
         int colcount = hdrVitals.getChildren().size() - 2;
         int maxcols = sldVitals.getCurpos() + 1;
@@ -481,61 +489,61 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
         pgVitals.setPageSize(Math.min(maxcols, colcount));
         onPaging$pgVitals();
     }
-    
+
     public void onScroll$sldVitals() {
         updatePaging();
     }
-    
+
     public void onPaging$pgVitals() {
         int maxCols = sldVitals.getCurpos() + 1;
         int cols = hdrVitals.getChildren().size() - 1;
         int col1 = pgVitals.getActivePage() * maxCols;
         int col2 = col1 + maxCols;
-        
+
         for (int col = 1; col < cols; col++) {
             ((Listheader) hdrVitals.getChildren().get(col)).setVisible(col > col1 && col <= col2);
         }
     }
-    
+
     public void onSelect$lstVitals() {
         setSelectedRow(lstVitals.getSelectedIndex());
     }
-    
+
     public void onSelectRange$datRange() {
         loadGrid();
     }
-    
+
     public void onSelect$cboUnits() {
         loadGrid();
     }
-    
+
     public void onCheck$chkGrid() {
         int w = chkGrid.isChecked() ? 1 : 0;
         chart.getXAxis().gridLineWidth = w;
         chart.getYAxis().gridLineWidth = w;
         chartData();
     }
-    
+
     public void onCheck$chkAge() {
         chartData();
     }
-    
+
     public void onCheck$chkPercentiles() {
         chartData();
     }
-    
+
     public void onCheck$chkZoom() {
         chart.options.chart.zoomType = chkZoom.isChecked() ? ZoomType.xy : null;
         chartData();
     }
-    
+
     /**
      * Called if the patient context change was canceled.
      */
     @Override
     public void canceled() {
     }
-    
+
     /**
      * Called if the patient context was committed.
      */
@@ -545,17 +553,17 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
         chkAge.setVisible(patient != null && dateToAge(today) < 37);
         loadGrid();
     }
-    
+
     /**
      * Called when a patient context change has been requested.
-     * 
+     *
      * @param silent If true, user interaction is not permitted.
      */
     @Override
     public String pending(boolean silent) {
         return null;
     }
-    
+
     /**
      * The CareWeb framework will call this method whenever the component becomes activated
      * (visible).
@@ -563,7 +571,7 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
     @Override
     public void onActivate() {
     }
-    
+
     /**
      * The CareWeb framework will call this method whenever the component becomes inactivated
      * (hidden).
@@ -571,21 +579,21 @@ public class Display extends Div implements PatientContext.IPatientContextEvent,
     @Override
     public void onInactivate() {
     }
-    
+
     /**
      * The CareWeb framework will call this method whenever the component is initially loaded.
-     * 
+     *
      * @param container Reference to the plugin's container.
      */
     @Override
     public void onLoad(PluginContainer container) {
     }
-    
+
     /**
      * The CareWeb framework will call this method whenever the component is unloaded.
      */
     @Override
     public void onUnload() {
     }
-    
+
 }
