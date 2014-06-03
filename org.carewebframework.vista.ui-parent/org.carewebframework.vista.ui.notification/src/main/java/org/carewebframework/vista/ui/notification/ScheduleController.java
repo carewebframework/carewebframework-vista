9c0eb9aed7601cfa79c95117cd0253d14a53ce24
Douglas Martin <dkmartin@regenstrief.org>
2014-06-03 08:03:24 -0400

    Fix cast error.

--- org.carewebframework.vista.ui-parent/org.carewebframework.vista.ui.notification/src/main/java/org/carewebframework/vista/ui/notification/ScheduleController.java
+++ org.carewebframework.vista.ui-parent/org.carewebframework.vista.ui.notification/src/main/java/org/carewebframework/vista/ui/notification/ScheduleController.java
@@ -1,8 +1,8 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
- * If a copy of the MPL was not distributed with this file, You can obtain one at 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
  * http://mozilla.org/MPL/2.0/.
- * 
+ *
  * This Source Code Form is also subject to the terms of the Health-Related Additional
  * Disclaimer of Warranty and Limitation of Liability available at
  * http://www.carewebframework.org/licensing/disclaimer.
@@ -13,8 +13,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.carewebframework.vista.mbroker.FMDate;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -28,6 +26,8 @@ import org.carewebframework.ui.zk.ListUtil;
 import org.carewebframework.ui.zk.PopupDialog;
 import org.carewebframework.ui.zk.PromptDialog;
 import org.carewebframework.ui.zk.ZKUtil;
+import org.carewebframework.vista.api.util.VistAUtil;
+import org.carewebframework.vista.mbroker.FMDate;
 
 import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Component;
@@ -43,36 +43,36 @@ import org.zkoss.zul.Window;
  * Controller for creating or editing a scheduled notification.
  */
 public class ScheduleController extends FrameworkController {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     private static final String DIALOG = ZKUtil.getResourcePath(ScheduleController.class) + "schedule.zul";
-    
+
     private NotificationService service;
-    
+
     private ScheduledNotification notification;
-    
+
     private final List<Recipient> recipients = new ArrayList<Recipient>();
-    
+
     private DateTimebox dtbDelivery;
-    
+
     private Combobox cboPriority;
-    
+
     private Textbox txtSubject;
-    
+
     private Textbox txtMessage;
-    
+
     private Textbox txtRecipients;
-    
+
     private Checkbox chkAssociate;
-    
+
     private Label lblPatient;
-    
+
     private Component pnlAssociate;
-    
+
     /**
      * Display the scheduled notification dialog modally.
-     * 
+     *
      * @param notification The scheduled notification to be modified. Specify null to create a new
      *            scheduled notification.
      * @return The modified or new scheduled notification, or null if the dialog was cancelled.
@@ -83,7 +83,7 @@ public class ScheduleController extends FrameworkController {
         Window dlg = PopupDialog.popup(DIALOG, args, true, false, true);
         return (ScheduledNotification) dlg.getAttribute("notification");
     }
-    
+
     /**
      * Initialize the dialog.
      */
@@ -91,13 +91,13 @@ public class ScheduleController extends FrameworkController {
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
         notification = (ScheduledNotification) arg.get("notification");
-        
+
         for (Priority priority : Priority.values()) {
             Comboitem item = new Comboitem(priority.toString(), priority.getImage());
             item.setValue(priority);
             cboPriority.appendChild(item);
         }
-        
+
         if (notification == null) {
             notification = new ScheduledNotification();
             notification.setPriority(Priority.LOW);
@@ -106,10 +106,10 @@ public class ScheduleController extends FrameworkController {
         } else {
             service.getScheduledNotificationRecipients(notification, recipients);
         }
-        
+
         populateForm();
     }
-    
+
     /**
      * Populate the dialog based on values from the scheduled notification.
      */
@@ -117,48 +117,48 @@ public class ScheduleController extends FrameworkController {
         dtbDelivery.setDate(notification.getDeliveryDate());
         dtbDelivery.setConstraint("no past");
         ListUtil.selectComboboxData(cboPriority, notification.getPriority());
-        
+
         txtSubject.setValue(notification.getSubject());
         txtMessage.setValue(StrUtil.fromList(service.getScheduledNotificationMessage(notification)));
-        
+
         if (notification.hasPatient()) {
             lblPatient.setValue(notification.getPatientName());
             chkAssociate.setChecked(true);
             chkAssociate.setValue(notification.getDfn());
         } else {
             IPatient patient = PatientContext.getActivePatient();
-            
+
             if (patient == null) {
                 pnlAssociate.setVisible(false);
             } else {
                 lblPatient.setValue(patient.getFullName() + " (" + patient.getMRN() + ")");
-                chkAssociate.setValue(patient.getDomainId());
+                chkAssociate.setValue(VistAUtil.parseIEN(patient));
             }
         }
-        
+
         updateRecipients();
     }
-    
+
     /**
      * Update the recipient text box based on the current recipient list.
      */
     private void updateRecipients() {
         StringBuilder sb = new StringBuilder();
-        
+
         for (Recipient recipient : recipients) {
             if (sb.length() > 0) {
                 sb.append("; ");
             }
-            
+
             sb.append(recipient.getName());
         }
-        
+
         txtRecipients.setText(sb.toString());
     }
-    
+
     /**
      * Validate entries.
-     * 
+     *
      * @return True if all entries successfully validated. False otherwise.
      */
     private boolean validate() {
@@ -171,29 +171,29 @@ public class ScheduleController extends FrameworkController {
         } else {
             return true;
         }
-        
+
         return false;
     }
-    
+
     /**
      * Displays a validation error next to the specified component.
-     * 
+     *
      * @param comp The component that failed validation.
      * @param key The key of the label to display.
      */
     private void wrongValue(Component comp, String key) {
         Clients.wrongValue(comp, Labels.getLabel(key));
     }
-    
+
     /**
      * Allows IOC container to inject notification service.
-     * 
+     *
      * @param service Notification service.
      */
     public void setNotificationService(NotificationService service) {
         this.service = service;
     }
-    
+
     /**
      * Update the scheduled notification with new input values and send to the server, then close
      * the dialog if successful.
@@ -206,7 +206,7 @@ public class ScheduleController extends FrameworkController {
             notification.setSubject(txtSubject.getValue());
             notification.setPriority((Priority) cboPriority.getSelectedItem().getValue());
             List<String> message = StrUtil.toList(txtMessage.getText());
-            
+
             if (service.scheduleNotification(notification, message, recipients)) {
                 root.setAttribute("notification", notification);
                 root.detach();
@@ -215,7 +215,7 @@ public class ScheduleController extends FrameworkController {
             }
         }
     }
-    
+
     /**
      * Show the recipients dialog.
      */
