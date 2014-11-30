bdf7c3a9d586cb423f045d9806162aed7f2331af
Douglas Martin <dkmartin@regenstrief.org>
2014-11-29 21:46:42 -0500

    Notification slide-down will now use Bootstrap alert themes.

--- org.carewebframework.vista.ui-parent/org.carewebframework.vista.ui.notification/src/main/java/org/carewebframework/vista/ui/notification/Priority.java
+++ org.carewebframework.vista.ui-parent/org.carewebframework.vista.ui.notification/src/main/java/org/carewebframework/vista/ui/notification/Priority.java
@@ -21,7 +21,7 @@ import org.zkoss.util.resource.Labels;
  * Notification priority.
  */
 public enum Priority {
-    HIGH("bullet_red.png", "red"), MEDIUM("bullet_yellow.png", "yellow"), LOW("bullet_green.png", "lightgreen");
+    HIGH("bullet_red.png", "error"), MEDIUM("bullet_yellow.png", "warning"), LOW("bullet_green.png", "success");
     
     private String image;
     
