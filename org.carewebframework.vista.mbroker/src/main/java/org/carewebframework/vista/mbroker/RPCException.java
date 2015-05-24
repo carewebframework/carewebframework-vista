509c407f8659bb7856fc6c686c690e8754ed169e
Douglas Martin <dkmartin@regenstrief.org>
2015-05-24 08:37:02 -0400

    Add an additional constructor to RPCException.

--- org.carewebframework.vista.mbroker/src/main/java/org/carewebframework/vista/mbroker/RPCException.java
+++ org.carewebframework.vista.mbroker/src/main/java/org/carewebframework/vista/mbroker/RPCException.java
@@ -15,24 +15,24 @@ import org.apache.commons.lang.StringUtils;
  * Wraps exceptions returned by an RPC.
  */
 public class RPCException extends RuntimeException {
-
+    
     private static final long serialVersionUID = 1L;
-
+    
     private int code = 0;
-
+    
     /**
      * Exception information parsed from the RPC return value.
      */
     private static class ExceptionInfo {
-
+        
         private int code;
-
+        
         private String text;
-
+        
         private ExceptionInfo(String text) {
             int i = text.indexOf(Constants.LINE_SEPARATOR);
             String first = i < 1 ? "" : text.substring(0, i);
-
+            
             if (StringUtils.isNumeric(first)) {
                 code = Integer.parseInt(first);
                 this.text = text.substring(++i);
@@ -41,7 +41,7 @@ public class RPCException extends RuntimeException {
             }
         }
     }
-
+    
     /**
      * Creates an exception based on the error text.
      *
@@ -50,17 +50,27 @@ public class RPCException extends RuntimeException {
     public RPCException(String text) {
         this(new ExceptionInfo(text));
     }
-
+    
+    /**
+     * Creates an RPC exception from an error code and text.
+     * 
+     * @param code The error code.
+     * @param text The error text.
+     */
+    public RPCException(int code, String text) {
+        super(text);
+        this.code = code;
+    }
+    
     /**
      * Creates an exception from the parsed text.
      *
      * @param value Exception information.
      */
     private RPCException(ExceptionInfo value) {
-        super(value.text);
-        this.code = value.code;
+        this(value.code, value.text);
     }
-
+    
     /**
      * Returns the error code.
      *
@@ -69,7 +79,7 @@ public class RPCException extends RuntimeException {
     public int getCode() {
         return code;
     }
-
+    
     @Override
     public String toString() {
         return code + ": " + super.toString();
