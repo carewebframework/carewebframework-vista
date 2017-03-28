fcaf36b03bc400753907d781cd1da201dffebb30
Douglas Martin <dkmartin@regenstrief.org>
2017-03-28 11:55:28 -0500

    MPL --> Apache license

--- org.carewebframework.vista.security-parent/org.carewebframework.vista.security.base/src/main/java/org/carewebframework/vista/security/base/SecurityDomainLoader.java
+++ org.carewebframework.vista.security-parent/org.carewebframework.vista.security.base/src/main/java/org/carewebframework/vista/security/base/SecurityDomainLoader.java
@@ -1,14 +1,27 @@
-/**
- * The contents of this file are subject to the Regenstrief Public License
- * Version 1.0 (the "License"); you may not use this file except in compliance with the License.
- * Please contact Regenstrief Institute if you would like to obtain a copy of the license.
+/*
+ * #%L
+ * carewebframework
+ * %%
+ * Copyright (C) 2008 - 2017 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * This Source Code Form is also subject to the terms of the Health-Related
+ * Additional Disclaimer of Warranty and Limitation of Liability available at
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
+ *      http://www.carewebframework.org/licensing/disclaimer.
  *
- * Copyright (C) Regenstrief Institute.  All Rights Reserved.
+ * #L%
  */
 package org.carewebframework.vista.security.base;
 
